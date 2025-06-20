aa4800a7ec4930ab348e70b65c44f69a04c5f92b
Timkaro <timkaro@i.ua>
2025-06-20 20:09:34 +0200

    fix errors

--- app/notes/[id]/error.tsx
+++ app/notes/[id]/error.tsx
@@ -5,5 +5,5 @@ type Props = {
 };
 
 export default function Error({ error }: Props) {
-  return <p>Could not fetch the list of notes. {error.message}</p>;
+  <p>Could not fetch note details. {error.message}</p>;
 }
